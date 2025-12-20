ed23a6e916b53f38de9e0c8aa65796f4ecd6468a
Wu Yu Jian <wyj@Wus-MacBook-Pro.local>
2025-12-20 22:51:54 +0800

    adds mac build

--- build.sh
+++ build.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 set -e
-cmake --preset default
-cd build && make -j$(nproc)
+cmake --preset default -DMLIR_DIR=/opt/homebrew/Cellar/llvm@20/20.1.8/lib/cmake/mlir
+
+# Use nproc on Linux, sysctl on macOS
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  NPROC=$(sysctl -n hw.ncpu)
+else
+  NPROC=$(nproc)
+fi
+
+cd build && make -j$NPROC
